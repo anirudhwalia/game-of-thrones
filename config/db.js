e348bf2310d900af14af6c7906e6fbe4dfe34898
Anirudh Walia <anirudh.walia300@gmail.com>
2020-12-21 18:27:33 +0530

    got app22

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db=config.get('mongoURI');
 const connectDB=async ()=>{
     try{
         
-        await mongoose.connect(process.env.MONGODB_URI || db,{
+        await mongoose.connect(db,{
             useNewUrlParser:true,
             useCreateIndex:true,
             useFindAndModify:false
